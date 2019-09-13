5fc1e6dc969722a7136ab98165411c29dc287436
faraday58 <jarodriguezvera@gmail.com>
2019-09-13 14:41:01 -0500

    Ejemplo Arrglos de objetos

---
+++ ArregloObjetos-G4/Chocolate.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ArregloObjetos_G4
+{
+    class Chocolate
+    {
+        private string sabor;
+        private string nombre;
+        private float precio;
+
+        public string Sabor { get => sabor; set => sabor = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public float Precio { get => precio; set => precio = value; }
+    }
+}
--- ArregloObjetos-G4/Program.cs
+++ ArregloObjetos-G4/Program.cs
@@ -1,15 +1,78 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ArregloObjetos_G4
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            
+
+            Chocolate[] chocolateArr = new Chocolate[3];
+            Chocolate CarlosV = new Chocolate();
+            CarlosV.Sabor = "Amargo";
+            CarlosV.Nombre = "Carlos V";
+            chocolateArr[0] = CarlosV;
+
+            Chocolate Crunch = new Chocolate();
+            Crunch.Sabor = "Vainilla";
+            Crunch.Nombre = "Crunch";
+            chocolateArr[1] = Crunch;
+
+            Chocolate Cacep = new Chocolate();
+            Cacep.Sabor = "Semi-Amargo";
+            Cacep.Nombre = "Cacep";
+            chocolateArr[2] = Cacep;
+            bool validaError = false; ;
+            do
+            {
+                try
+                {
+                    validaError = false;
+                    Console.Clear();
+                    for (int i = 0; i < chocolateArr.Length; i++)
+                    {
+
+                        Console.WriteLine(" Nombre: {0} \n Sabor: {1} \n", chocolateArr[i].Nombre, chocolateArr[i].Sabor);
+                        Console.WriteLine("Ingresa el precio del chocolate");
+                        chocolateArr[i].Precio = float.Parse(Console.ReadLine());
+                        if (chocolateArr[i].Precio <= 0)
+                        {
+                            throw new ApplicationException("El precio debe ser mayor a cero");
+                        }
+
+                        Console.WriteLine("Precio {0}", chocolateArr[i].Precio);
+                    }
+
+                }
+
+                catch (ApplicationException error)
+                {
+                    validaError = true;
+                    Console.WriteLine(error.Message);
+                }
+
+                catch (IndexOutOfRangeException error)
+                {
+                    validaError = true;
+                    Console.WriteLine(error.Message);
+                }
+                catch (FormatException)
+                {
+                    validaError = true;
+                    Console.WriteLine("No se ingresó un número");
+                }
+                catch
+                {
+                    validaError = true;
+                    Console.WriteLine("Hubo un error");
+                }
+
+
+            } while (validaError);
+            
+
+
         }
     }
 }
